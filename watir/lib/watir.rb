9bb9120c726e45413f2fd9716758908bcab56093
Jarmo Pertman <jarmo.p@gmail.com>
2012-09-17 22:52:50 +0300

    Improved watir/loader to support watir-webdriver too.

--- watir/lib/watir.rb
+++ watir/lib/watir.rb
@@ -1,1 +1,2 @@
-require 'watir/loader'
+require File.expand_path('watir/version', File.dirname(__FILE__))
+require File.expand_path('watir/loader', File.dirname(__FILE__))
--- watir/lib/watir/loader.rb
+++ watir/lib/watir/loader.rb
@@ -1,2 +1,49 @@
-require 'watir-classic'
-require 'watir/version'
+module Watir
+  class Browser
+    class << self
+      def new(browser=nil, *args)
+        if browser && browser.to_sym != :ie && Watir.driver == :classic
+          Watir.driver = :webdriver 
+        end
+        Watir.load_driver
+
+        if Watir.driver == :webdriver
+          # remove this class method for WebDriver to avoid endless loop
+          singleton_class = class << self; self end
+          singleton_class.send :remove_method, :new
+        end
+
+        new browser, *args
+      end
+    end
+  end
+
+  class << self
+    def load_driver
+      require "watir-#{driver}"
+    rescue LoadError
+      puts "watir-#{driver} gem is missing. Install it with the following command:"
+      puts "  gem install watir-#{driver}"
+    end
+
+    def driver
+      @driver || ENV["WATIR_DRIVER"] || default_driver
+    end
+
+    def driver=(driver)
+      allowed_drivers = %w[webdriver classic]
+      unless allowed_drivers.map(&:to_sym).include?(driver.to_sym)
+        raise "Supported drivers are #{allowed_drivers.join(", ")}." 
+      end
+      @driver = driver
+    end
+
+    def default_driver
+      if ENV['OS'] == 'Windows_NT'
+        :classic
+      else
+        :webdriver
+      end
+    end
+  end
+end
